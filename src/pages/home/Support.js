141a25d1c650fa28fe19398fb0e99a311541e860
MahbubMorshed <mahbub.bogra30@gmail.com>
2022-05-28 12:39:12 -0700

    contact section added

--- src/pages/home/Support.js
+++ src/pages/home/Support.js
@@ -1,7 +1,73 @@
 import React from "react";
+import support from "../../images/support.jpg";
+import { RiChatSettingsLine } from "react-icons/ri";
+import { SiMaildotru } from "react-icons/si";
+import { HiDocumentText } from "react-icons/hi";
+import { FaDownload } from "react-icons/fa";
+import { FaQuestionCircle } from "react-icons/fa";
+import { RiAwardFill } from "react-icons/ri";
 
 const Support = () => {
-  return <div>Support</div>;
+  return (
+    <div className="mt-5 mx-2">
+      <h1 className="text-secondary text-center text-4xl font-bold mb-5">
+        GET IN TOUCH
+      </h1>
+      <img src={support} />
+      <div className="grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-2 my-5">
+        <div className="card bg-gray-300 hover:bg-secondary hover:text-neutral">
+          <div className="card-body">
+            <RiChatSettingsLine className="text-4xl text-center mx-auto text-primary" />
+            <h3 className="px-0 text-center">CHAT NOW!</h3>
+            <small className="text-center">
+              Get quick assistance with online chat. (English only)
+            </small>
+          </div>
+        </div>
+        <div className="card bg-gray-300 hover:bg-secondary hover:text-neutral">
+          <div className="card-body">
+            <SiMaildotru className="text-4xl text-center mx-auto text-primary" />
+            <h3 className="px-0 text-center">CONTACT US</h3>
+            <small className="text-center">How can we help?</small>
+          </div>
+        </div>
+        <div className="card bg-gray-300 hover:bg-secondary hover:text-neutral">
+          <div className="card-body">
+            <HiDocumentText className="text-4xl text-center mx-auto text-primary" />
+            <h3 className="px-0 text-center">PRODUCT REGISTRATION</h3>
+            <small className="text-center">Register your new product.</small>
+          </div>
+        </div>
+        <div className="card bg-gray-300 hover:bg-secondary hover:text-neutral">
+          <div className="card-body">
+            <FaDownload className="text-4xl text-center mx-auto text-primary" />
+            <h3 className="px-0 text-center">DOWNLOAD CENTER</h3>
+            <small className="text-center">
+              Get the latest driver, software and product assets.
+            </small>
+          </div>
+        </div>
+        <div className="card bg-gray-300 hover:bg-secondary hover:text-neutral">
+          <div className="card-body">
+            <FaQuestionCircle className="text-4xl text-center mx-auto text-primary" />
+            <h3 className="px-0 text-center">FAQ</h3>
+            <small className="text-center">
+              Find answers to the most asked questions.
+            </small>
+          </div>
+        </div>
+        <div className="card bg-gray-300 hover:bg-secondary hover:text-neutral">
+          <div className="card-body">
+            <RiAwardFill className="text-4xl text-center mx-auto text-primary" />
+            <h3 className="px-0 text-center">WARRANTY</h3>
+            <small className="text-center">
+              Learn about our Product Warranty Policy.
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Support;
--- src/pages/home/WhatsNew.js
+++ src/pages/home/WhatsNew.js
@@ -2,11 +2,14 @@ import React from "react";
 import new1 from "../../images/3090ti.jpg";
 import new2 from "../../images/1650.jpg";
 import new3 from "../../images/2060.jpg";
+import new4 from "../../images/3050.jpg";
+import new5 from "../../images/3090.jpg";
+import new6 from "../../images/3070.jpg";
 import { Link } from "react-router-dom";
 
 const WhatsNew = () => {
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 ">
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 mx-2">
       <div class="card bg-secondary text-neutral shadow-xl mt-5">
         <figure>
           <img src={new1} alt="Shoes" className="pt-3" />
@@ -77,6 +80,87 @@ const WhatsNew = () => {
             <li>1920 Cores</li>
             <li>Boost: 1680 MHz</li>
             <li>6GB GDDR6 / 14.0 Gbps / 192-bit</li>
+          </ul>
+          <div class="card-actions justify-end">
+            <Link to="/purchase">
+              <div class="badge badge-primary text-neutral">Best Selling</div>
+            </Link>
+            <Link to="/purchase">
+              <div class="badge badge-outline-primary">Purchase</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div class="card bg-secondary text-neutral shadow-xl mt-5">
+        <figure>
+          <img src={new4} alt="Shoes" className="pt-3" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">
+            ZOTAC GAMING GeForce RTX 3050 AMP
+            <div class="badge badge-primary text-neutral">NEW</div>
+          </h2>
+          <p>
+            <small>SKU: ZT-A30500F-10M</small>
+          </p>
+          <ul>
+            <li>2560 Cores</li>
+            <li>Boost: 1830 MHz</li>
+            <li>8GB GDDR6 / 14 Gbps / 128-bit</li>
+          </ul>
+          <div class="card-actions justify-end">
+            <Link to="/purchase">
+              <div class="badge badge-primary text-neutral">Best Selling</div>
+            </Link>
+            <Link to="/purchase">
+              <div class="badge badge-outline-primary">Purchase</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div class="card bg-secondary text-neutral shadow-xl mt-5">
+        <figure>
+          <img src={new5} alt="Shoes" className="pt-3" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">
+            ZOTAC GAMING GeForce RTX 3090 Trinity OC
+            <div class="badge badge-primary text-neutral">NEW</div>
+          </h2>
+          <p>
+            <small>SKU: ZT-A30900J-10P</small>
+          </p>
+          <ul>
+            <li>10496 Cores</li>
+            <li>Boost: 1710 MHz</li>
+            <li>24GB GDDR6X / 19.5 Gbps / 384-bit</li>
+          </ul>
+          <div class="card-actions justify-end">
+            <Link to="/purchase">
+              <div class="badge badge-primary text-neutral">Best Selling</div>
+            </Link>
+            <Link to="/purchase">
+              <div class="badge badge-outline-primary">Purchase</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div class="card bg-secondary text-neutral shadow-xl mt-5">
+        <figure>
+          <img src={new6} alt="Shoes" className="pt-3" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">
+            ZOTAC GAMING GeForce RTX 3070 Twin Edge LHR
+            <div class="badge badge-primary text-neutral">NEW</div>
+          </h2>
+          <p>
+            <small>SKU: ZT-A30700E-10PLHR</small>
+          </p>
+          <ul>
+            <li>5888 Cores</li>
+            <li>Boost: 1725 MHz</li>
+            <li>8GB GDDR6 / 14 Gbps / 256-bit</li>
           </ul>
           <div class="card-actions justify-end">
             <Link to="/purchase">
